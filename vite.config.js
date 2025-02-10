b0560769afef3980d9ceca45cabb998c0d78075a
vickyee-dev <victorpeter0540@gmail.com>
2025-02-10 21:50:36 +0300

    hunting_app

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173, // Render assigns a dynamic port
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: ["hunting-app.onrender.com"], // Allow Render's domain
+  },
 });
